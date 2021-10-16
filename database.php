bf03416572124b4bd480a7a48947464f914b6c3f
Yashitha Nadiranga <35962717+YashithaNadiranga@users.noreply.github.com>
2021-10-16 12:58:05 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "localhost";
 	$username = "yashitha_love";
-	$password = "Yashitha@123";
+	$password = "-";
 	$db="yashitha_love";
 	// $conn = mysqli_connect($servername, $username, $password,$db);
 
